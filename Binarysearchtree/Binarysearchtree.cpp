54e8c959eb25757b86f0b1d687a72afe652727ef
ImamDwiYanPermana <imampermana224@gmail.com>
2023-07-01 18:58:54 +0700

    unutk mencari current node dari suatu node yang di tentukan

--- Binarysearchtree/Binarysearchtree.cpp
+++ Binarysearchtree/Binarysearchtree.cpp
@@ -50,4 +50,8 @@ public:
 			parent->rightchild = newNode; //Make the right child of the parent pointto thenew node
 		}
 	}
+	void search(string element, Node*& parent, Node*& currentNode) { //This function searches the cuurentNode of the specified Node current Node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+	}
 };
